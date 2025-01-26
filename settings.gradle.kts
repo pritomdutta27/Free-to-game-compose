538ad394970193c66e9b698ac7399c7a02184679
Pritom Dutta <32538483+pritomdutta27@users.noreply.github.com>
2025-01-26 22:19:53 +0600

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Free to game Compose"
+include(":app")
+include(":features:gamelist:data")
+include(":features:gamelist:domain")
+include(":features:gamelist:ui")
